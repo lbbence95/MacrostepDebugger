0ebde8ac202fdc46501d437173c89196c0470560
Liget-BigPC\Liget <lbbence95@gmail.com>
2020-08-16 17:34:22 +0200

    Removed unnecessary lines.

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,15 +1,11 @@
 # Represents the controller module of the macrostep based debugger
 
-
 import src.controller.repository as msteprepo
-
 
 def InitializeDB():
     """Initializes a new database.
     """
-
     msteprepo.InitializeDB()
-
 
 def CanNodeMoveNext(infra_id, node_id):
     """Decides if the given node in the given infrastructure is permited to move to the next breakpoint.
@@ -29,7 +25,6 @@ def CanNodeMoveNext(infra_id, node_id):
     else:
         return False
 
-
 def InfraExists(infra_id):
     """Decides if the given infrastructure is already registered.
 
@@ -46,7 +41,6 @@ def InfraExists(infra_id):
         return False
     else:
         return True
-
 
 def IsNewBreakpointNext(infra_id, node_id, bp_id):
     """Determines if the received breakpoint is the next breakpoint.
@@ -69,7 +63,6 @@ def IsNewBreakpointNext(infra_id, node_id, bp_id):
     except IndexError:
         return False
 
-
 def NodeExists(infra_id, node_id):
     """Checks if the given node within the given infrastrucutre exists or not.
 
@@ -88,7 +81,6 @@ def NodeExists(infra_id, node_id):
     else:
         return True
 
-
 def RegisterInfrastructure(infra_id, infra_name, registration_date):
     """Registers a new infrastructure (create).
 
@@ -104,8 +96,7 @@ def RegisterInfrastructure(infra_id, infra_name, registration_date):
         # TO-DO: Throw some error
         pass
 
-
-def RegisterNode(infra_id, node_id, node_name, registration_timestamp, bp_id):
+def RegisterNode(infra_id, node_id, node_name, registration_timestamp, bp_id, public_ip):
     """Registers a new node (create).
 
     Args:
@@ -117,11 +108,10 @@ def RegisterNode(infra_id, node_id, node_name, registration_timestamp, bp_id):
     """
 
     if (infra_id != None and infra_id != "") and (node_id != None and node_id != "") and registration_timestamp != None:
-        msteprepo.RegisterNode(infra_id, node_id, node_name, registration_timestamp, bp_id)
+        msteprepo.RegisterNode(infra_id, node_id, node_name, registration_timestamp, bp_id, public_ip)
     else:
         # TO-DO: Throw some error
         pass
-
 
 def UpdateNodeBreakpoint(infra_id, node_id):
     """Updates a given node (update). This method updates the breakpoint to the next breakpoint, and updates the permission of moving to the next breakpoint to False (0).
@@ -137,7 +127,6 @@ def UpdateNodeBreakpoint(infra_id, node_id):
         # TO-DO: Throw some error
         pass
 
-
 def MoveNodeToNext(infra_id, node_id):
     """Updates the given nodes permission of moving to the next breakpoint to True.
 
@@ -145,11 +134,9 @@ def MoveNodeToNext(infra_id, node_id):
         infra_id (string): An infrastructure ID.
         node_id (string): A node ID.
     """
-
     msteprepo.UpdateSpecificNodeMoveNext(infra_id, node_id)
 
-
-def RegisterBreakpoint(infra_id, node_id, registration_timestamp, bp_num, node_data):
+def RegisterBreakpoint(infra_id, node_id, registration_timestamp, bp_num, node_data, bp_tag):
     """Registers a new breakpoint (create).
 
     Args:
@@ -158,10 +145,11 @@ def RegisterBreakpoint(infra_id, node_id, registration_timestamp, bp_num, node_d
         registration_timestamp (datetime): A timestamp of when the breakpoint was registered.
         bp_num (int): The breakpoint's unique number.
         node_data (string): A JSON string.
+        bp_tag (string): A description of the breakpoint (e.g.: tags).
     """
 
     if (infra_id != None and infra_id != "") and (node_id != None and node_id != "") and bp_num > 0:
-        msteprepo.RegisterBreakpoint(infra_id, node_id, registration_timestamp, bp_num, node_data)
+        msteprepo.RegisterBreakpoint(infra_id, node_id, registration_timestamp, bp_num, node_data, bp_tag)
     else:
         # TO-DO: Throw some error
         pass
