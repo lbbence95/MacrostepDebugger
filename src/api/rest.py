45b20557a308fde2f42b07427815f0b5e795ce81
Liget-BigPC\Liget <lbbence95@gmail.com>
2020-08-18 19:16:16 +0200

    Removed validation.py. Functions moved to REST.

--- src/api/rest.py
+++ src/api/rest.py
@@ -6,7 +6,6 @@ from datetime import datetime
 import json
 
 import src.util.consolelogs as mstep_conlogger
-import src.util.validation as mstepvalidation
 import src.controller.controller as mstepcontroller
 
 app = Flask(__name__)
@@ -28,7 +27,7 @@ def APIdatacollector():
 
     # A successful request contains a valid JSON, with necessary keys and values. The breakpoint also has to be a valid breakpoint (breakpoint number either 1 if this is 
     # the first breakpoint, or the next if the node already exists in the infrastructure).
-    successful = mstepvalidation.ValidateJSON(request_data) and mstepvalidation.ValidateNecessaryKeysExists(request_data) and mstepvalidation.ValidateJSONValues(request_data)
+    successful = ValidateJSON(request_data) and ValidateNecessaryKeysExists(request_data) and ValidateJSONValues(request_data)
 
     infra_id, node_id, bp_id = None, None, None
 
@@ -142,3 +141,87 @@ def GetMoveToNextBP(infraID, nodeID):
 #        raise RuntimeError('Not running with the Werkzeug Server')
 #    func()
 #    pass
+
+def ValidateJSONValues(request_data):
+    """Validates if the necessary keys have proper values.
+
+    Args:
+        request_data (request): A request containing the JSON string.
+
+    Returns:
+        bool: True if the values are acceptable, otherwise False.
+    """
+    
+    json_data = json.loads(request_data.get_data())
+
+    try:
+        infra_ok = isinstance(json_data['infraData']['infraID'], str) and len(json_data['infraData']['infraID']) > 0
+        node_ok = isinstance(json_data['nodeData']['nodeID'], str) and len(json_data['nodeData']['nodeID']) > 0
+        bp_ok = isinstance(json_data['bpData']['bpNum'], int) and json_data['bpData']['bpNum'] > 0
+
+        if infra_ok and node_ok and bp_ok:
+            return True
+        else:
+            return False
+    except TypeError:
+        return False
+
+def ValidateJSON(request_data):
+    """Checks if the given string is a valid JSON string.
+
+    Args:
+        request_data (string): A request containing the JSON string.
+
+    Returns:
+        bool: True if the string is a valid, False if the string is an invalid JSON.
+    """
+    try:
+        json.loads(request_data.get_data())
+    except ValueError:
+        return False
+    return True
+
+def ValidateNecessaryKeysExists(request_data):
+    """Checks if the JSON string contains the necessary keys.
+
+    Args:
+        jsonData (string): The JSON string to validate.
+    
+    Returns:
+        bool: True if the necessary keys exist in the JSON string. Otherwise False.
+    """
+
+    necessary_data = ({'infraData', 'bpData', 'nodeData'})
+    json_data = request_data.get_json()
+
+    ### TO-DO: refactoring
+    try:
+        # Infrastructure related data
+        json_data['infraData']['infraID']
+        json_data['infraData']['infraName']
+
+        # Local breakpoint related data
+        json_data['bpData']['bpNum']
+        json_data['bpData']['bpTag']
+
+        # Node related data
+        request_data.remote_addr
+        json_data['nodeData']['nodeID']
+        json_data['nodeData']['nodeName']
+        json_data['nodeData']
+
+        return True
+    except KeyError:
+        return False
+
+    if necessary_data <= set(json_data):
+        if (
+            {'infraID', 'infraName'} <= set(json_data['infraData']) and
+            {'bpNum'} <= set(json_data['bpData']) and
+            {'nodeID', 'nodeName'} <= set(json_data['nodeData'])
+        ):
+           return True 
+        else:
+            return False
+    else:
+        return False
