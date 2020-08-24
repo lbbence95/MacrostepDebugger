e53dfb4dc446070fd94a0b5469d1b06e8ae0d9e5
Liget-BigPC\Liget <lbbence95@gmail.com>
2020-08-24 14:21:12 +0200

    Initialization trough the controller. More detailed option descriptions.

--- src/debugger.py
+++ src/debugger.py
@@ -2,6 +2,7 @@
 
 import src.api.rest as msteprest
 import src.util.consolelogs as mstep_conlogger
+import src.controller.controller as mstepcontroller
 
 import argparse
 
@@ -14,15 +15,15 @@ group_basic.add_argument('-ln','--nodes', action='store_true', help='list manage
 
 # Service start
 group_basic.add_argument('-s','--start', action='store_true', help='start the macrostep debugger service (can be used with -p)')
-parser.add_argument('-p', '--port', type=str, metavar='', help='macrostep debugger service port (default is 5000)')
+parser.add_argument('-p', '--port', type=str, metavar='Portnumber', help='macrostep debugger service port (default is 5000)')
 
 # Details
-parser.add_argument('-dn','--detailsnode', type=str, metavar='', help='show details of the given node (Important! Used after option -di, cannot be used on its own)')
-parser.add_argument('-di','--detailsinfra', type=str, metavar='', help='show details of the given infrastructure.')
+parser.add_argument('-dn','--detailsnode', type=str, metavar='NodeID', help='show details of the given node (Important! Can be used after option -di, cannot be used on its own)')
+parser.add_argument('-di','--detailsinfra', type=str, metavar='InfraID', help='show details of the given infrastructure.')
 
 # Update
-parser.add_argument('-i', '--infra', type=str, metavar='', help='permit a node in the given infrastructure to move to the next breakpoint')
-parser.add_argument('-n','--node', type=str, metavar='', help='a node ID (Important! Used after option -i, cannot be used on its own)')
+parser.add_argument('-i', '--infra', type=str, metavar='InfraID', help='permit a node in the given infrastructure to move to the next breakpoint')
+parser.add_argument('-n','--node', type=str, metavar='NodeID', help='a node ID (Important! Can be used after option -i, cannot be used on its own)')
 
 args = parser.parse_args()
 
@@ -30,34 +31,37 @@ if __name__ == "__main__":
 
     # -li: List infrastructures
     if args.infrastructures == True:
-        msteprest.mstep_conlogger.PrintManagedInfras()
+        mstep_conlogger.PrintManagedInfras()
 
     # -ln: List nodes
     elif args.nodes == True:
-        msteprest.mstep_conlogger.PrintManagedNodes()
+        mstep_conlogger.PrintManagedNodes()
     
-    # -di:
+    # -di and -dn: Infrastructure details
     elif args.detailsinfra != None:
-
         if args.detailsnode != None:
             mstep_conlogger.PrintNode(args.detailsinfra, args.detailsnode)
         else:
             mstep_conlogger.PrintInfra(args.detailsinfra)
+    elif args.detailsnode != None:
+        print('Use "-dn" after option "-di"!')
     
-    # -i and -n
-    elif args.infra != None and args.node != None:
-
-        if msteprest.mstepcontroller.NodeExists(args.infra, args.node) == True:
-            # TO-DO: Refactoring, forward CLI request directly to the controller or to a seperate CLI handler...
-            msteprest.mstepcontroller.MoveNodeToNext(args.infra, args.node)
-            # TEST
-            mstep_conlogger.PrintNode(args.infra, args.node)
+    # -i and -n: Permit next breakpoint
+    elif args.infra != None:
+        if args.node != None:
+            if mstepcontroller.NodeExists(args.infra, args.node) == True:
+                mstepcontroller.MoveNodeToNext(args.infra, args.node)
+                mstep_conlogger.PrintNode(args.infra, args.node)
+            else:
+                print('No node with ID "{}" exists in infrastructure with ID "{}"!'.format(args.node, args.infra))
         else:
             mstep_conlogger.PrintInfra(args.infra)
+    elif args.node != None:
+        print('Use "-n" after option "-i"!')
     
-    # -s: Start the service
-    if args.start == True:
-        msteprest.Init()
+	# -s: Start the service
+    elif args.start == True:
+        mstepcontroller.Initialize()
 
         if args.port != None:
             try:
