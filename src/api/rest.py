8dfa1bc7e7a38424b8181ee142874f4942d115e5
Liget-BigPC\Liget <lbbence95@gmail.com>
2020-09-14 12:43:11 +0200

    Included console logging functions.

--- src/api/rest.py
+++ src/api/rest.py
@@ -1,11 +1,9 @@
-# Represents API functions
+# Represents REST API functions
+
+import src.controller.controller as mstepcontroller
+import datetime, json
 
 from flask import Flask, request, jsonify
-
-import json
-
-import src.util.consolelogs as mstep_conlogger
-import src.controller.controller as mstepcontroller
 
 app = Flask(__name__)
 
@@ -14,7 +12,7 @@ def APIdatacollector():
     """API site receiving data from VM endpoints.
 
     Returns:
-        string: An HTTP response with a JSON string describing the result.
+        response: A Flask response object with the JSON describing the result.
     """
 
     request_data = request
@@ -24,39 +22,39 @@ def APIdatacollector():
     valid = ValidateJSON(request_data)
 
     if valid == False:
-        mstep_conlogger.PrintConsoleInvalidData()
+        PrintConsoleInvalidData()
         return jsonify({'success':False, 'message':'Invalid JSON.', 'code':400}), 400, {'ContentType':'application/json'}
     else:
         valid = valid and ValidateNecessaryKeysExists(request_data.get_json())
 
         if valid == False:
-            mstep_conlogger.PrintConsoleInvalidData()
+            PrintConsoleInvalidData()
             return jsonify({'success':False, 'message':'Missing or invalid JSON keys and/or values.', 'code':422}), 422, {'ContentType':'application/json'}
         else:
             valid = valid and ValidateJSONValues(request_data)
 
-            if valid == False:
-                mstep_conlogger.PrintConsoleInvalidData()
+            if valid == False or valid == None:
+                PrintConsoleInvalidData()
                 return jsonify({'success':False, 'message':'Missing or invalid JSON values.', 'code':422}), 422, {'ContentType':'application/json'}
 
     if valid == True:
         # The request is valid and contains the necessary data. Passing JSON to controller.          
         result = mstepcontroller.ProcessJSON(request_data.remote_addr, request_data.get_json())
         if result[0] == 200:
-            mstep_conlogger.PrintConsoleRequestData(request_data) 
+            PrintConsoleRequestData(request_data) 
         else:
-            mstep_conlogger.PrintConsoleInvalidData()
+            PrintConsoleInvalidData()
 
         return jsonify({'code':result[0], 'message':result[1], 'success':result[2]}), result[0], {'ContentType':'application/json'}
     else:
         # The request was invalid
-        mstep_conlogger.PrintConsoleInvalidData()
+        PrintConsoleInvalidData()
         return jsonify({'success':False, 'message':'Invalid JSON, invalid keys or invalid values.', 'code':400}), 400, {'ContentType':'application/json'}
 
 @app.route("/MSTEP_API/Next/<infraID>/<nodeID>", methods=['GET'])
 def GetMoveToNextBP(infraID, nodeID):
     
-    # Check if infra and node exist, if the node can move to the next breakpoint
+    # Check if infra and node exists, also if the node can move to the next breakpoint
     if mstepcontroller.InfraExists(infraID) == True and mstepcontroller.NodeExists(infraID, nodeID) == True and mstepcontroller.CanNodeMoveNext(infraID, nodeID) == True:
         return json.dumps({'success':True, 'next':True}), 200, {'ContentType':'application/json'}
     else:
@@ -137,7 +135,6 @@ def ValidateNecessaryKeysExists(json_data):
         json_data['bpData']['bpTag']
 
         # Node related data
-        # request_data.remote_addr
         json_data['nodeData']['nodeID']
         json_data['nodeData']['nodeName']
         json_data['nodeData']['nodeIP']
@@ -158,3 +155,50 @@ def ValidateNecessaryKeysExists(json_data):
             return False
     else:
         return False
+
+def PrintConsoleInvalidData():
+    """Prints a message stating the received data were invalid.
+    """
+    print('\r\n*** ({}) Received INVALID breakpoint data.'.format(datetime.datetime.now().strftime('%Y.%m.%d. %H:%M:%S.%f')[:-3]))
+
+def PrintConsoleRequestData(request_data):
+    """Prints the received VM data to the console.
+
+    Args:
+        request_data (request): A request containing a JSON string.
+    """
+
+    json_data = request_data.get_json()
+ 
+    # Infrastructure related data
+    infra_id = json_data['infraData']['infraID']
+    infra_name = json_data['infraData']['infraName']
+
+    # Local breakpoint related data
+    bp_id = json_data['bpData']['bpNum']
+    bp_tag = json_data['bpData']['bpTag']
+
+    # Node related data
+    node_publicIP = request_data.remote_addr
+    node_id = json_data['nodeData']['nodeID']
+    node_name = json_data['nodeData']['nodeName']
+    node_data = json_data['nodeData']
+
+    # Printing received information
+    print('\r\n*** ({}) Received VALID breakpoint data.'.format(datetime.datetime.now().strftime('%Y.%m.%d. %H:%M:%S.%f')[:-3]))
+
+    print('*** From infrastructure with ID: {}'.format(infra_id))
+    print('*** From infrastructure with Name: {}'.format(infra_name))
+
+    print('*** From node with ID: {}'.format(node_id))
+    print('*** From node with Name: {}'.format(node_name))
+
+    print('*** Local breakpoint Number: {}'.format(bp_id))
+    print('*** Local breakpoint description: {}'.format(bp_tag))
+
+    print('*** Node Data:')
+
+    for dataKey in node_data:
+        print('*** {}: {}'.format(dataKey, node_data[dataKey]))
+
+    print('*** nodePublicIP: {}\r\n'.format(node_publicIP))
