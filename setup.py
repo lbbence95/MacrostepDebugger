ec35ccc662251db0be8f7a802da00c3a5e0fb57a
Liget-BigPC\Liget <lbbence95@gmail.com>
2022-06-28 09:03:47 +0200

    New setup.py file.

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env -e python
 
-# Macrostep debugger v0.4 (2021.06.)
+# Macrostep debugger v0.6 (2022.06.)
 
 import setuptools
 
 setuptools.setup(
     name='Cloud-based Macrostep Debugger',
-    version='0.4',
+    version='0.6',
     author='ELKH SZTAKI-LPDS',
     packages=[
         'api',
+		'bpgen',
         'controller',
-		'controller.orchestratorhandler',
-		'controller.orchestratorhandler.plugins',
+        'controller.orchestratorhandler',
+        'controller.orchestratorhandler.plugins',
         'data',
         'util'
     ],
     scripts=[
         'bin/debugger',
         'bin/debugger-app',
+		'bin/debugger-generate',
         'bin/debugger-list',
         'bin/debugger-step',
         'bin/debugger-play'
